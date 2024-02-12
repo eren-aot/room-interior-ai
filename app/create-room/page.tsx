a2aa1824c4f0475b80d80f26907dd5ecbdf88434
wolfgunblood <jalajdorai100@gmail.com>
2024-02-12 22:38:28 +0530

    done with form

--- app/create-room/page.tsx
+++ app/create-room/page.tsx
@@ -2,16 +2,18 @@ import RoomFormPage from '@/components/RoomForm'
 import React from 'react'
 
 const CreateRoomPage = () => {
-  return (
-    <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-        <div className='flex'>
-            <RoomFormPage />
-            <div>
-                Hello World
+    return (
+        <div className='max-w-7xl mx-auto px-6 md:px-12 xl:px-6'>
+            <div className='relative pt-80 sm:pt-24 ml-auto flex justify-center items-center h-screen'>
+                <div className='w-full flex justify-between'>
+                    <RoomFormPage />
+                    <div>
+                        Hello World
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default CreateRoomPage
--- components/RoomForm.tsx
+++ components/RoomForm.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form'
 import { RoomAISchema } from '@/schemas/schemas'
@@ -5,6 +6,7 @@ import { z } from "zod";
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Input } from '@/components/ui/input';
+import { Button } from './ui/button';
 
 type RoomInput = z.infer<typeof RoomAISchema>;
 
@@ -43,10 +45,10 @@ const RoomFormPage = () => {
                         </FormItem>
                     )}
                 />
-                <FormField 
+                <FormField
                     control={form.control}
                     name='style'
-                    render={({field}) => (
+                    render={({ field }) => (
                         <FormItem>
                             <FormLabel>
                                 Style
@@ -57,6 +59,41 @@ const RoomFormPage = () => {
                         </FormItem>
                     )}
                 />
+                <FormField
+                    control={form.control}
+                    name='materials'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>
+                                Style
+                            </FormLabel>
+                            <FormControl>
+                                <Input placeholder='materials' {...field} />
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+                {/* <FormField
+                    name='prompt' 
+                /> */}
+                <FormField
+                    control={form.control}
+                    name='aspect_ratio'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>
+                                Aspect Ratio
+                            </FormLabel>
+                            <FormControl>
+                                <Input placeholder='Aspect Ratio' {...field} />
+                            </FormControl>
+                        </FormItem>
+                    )}
+
+                />
+                <Button type='submit'>
+                    Generate Images
+                </Button>
             </form>
         </Form>
     )
